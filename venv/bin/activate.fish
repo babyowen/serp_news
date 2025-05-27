9210d5cab35078ee04c0bd77ec291079ebdd6af2
babyowen <kakaba@me.com>
2025-05-27 17:01:15 +0800

    5-27

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/liuliang/python/serp_news/venv"
+set -gx VIRTUAL_ENV "/Users/babyowen/python/serp_news/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
